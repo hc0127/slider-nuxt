347253d117f3c7e74c7bb312689c28a951844e02
heracles0127 <heracles2756@gmail.com>
2023-11-28 20:34:13 +0200

    integrating with datas

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="slider-content">
-    <swiper :loop="true" :slidesPerView="4" :spaceBetween="30" :watchSlidesProgress="true" :autoplay="{
-      delay: 5000,
-      disableOnInteraction: false,
-    }" :navigation="true" :modules="modules" @autoplayTimeLeft="onAutoplayTimeLeft" class="mySlider">
-      <div class="slider-description-content"></div>
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-      <swiper-slide>Slide 4</swiper-slide>
-      <swiper-slide>Slide 5</swiper-slide>
-      <swiper-slide>Slide 6</swiper-slide>
+    <swiper ref="refSwiper" :loop="true" :slidesPerView="4" :spaceBetween="30" :watchSlidesProgress="true"
+      :navigation="true" :modules="modules" @autoplayTimeLeft="onAutoplayTimeLeft" @slideChange="handleSlideChange"
+      :autoplay="{
+        delay: 3000,
+        disableOnInteraction: false,
+      }" class="mySlider">
+      <div class="slider-description-content">
+        <h1>Benefits</h1>
+        <h2 class="slider_description_title">{{ datas[activeIndex].title }}</h2>
+        <p class="slider_description">{{ datas[activeIndex].description }}</p>
+      </div>
+      <swiper-slide v-for="(data, index) in datas" :key=index><img :src=data.img :alt=data.img /></swiper-slide>
       <template #container-end>
         <div class="autoplay-progress">
           <svg viewBox="0 0 64 64" ref="progressCircle">
@@ -42,8 +43,22 @@ export default {
     Swiper,
     SwiperSlide,
   },
-
-  setup() {
+  props: {
+    datas: []
+  },
+  data() {
+    return {
+      activeIndex: 0,
+      modules: [Autoplay, Navigation],
+    }
+  },
+  methods: {
+    handleSlideChange(swiper) {
+      const activeIndex = swiper.activeIndex && (swiper.previousRealIndex + 1) % this.datas.length;
+      this.activeIndex = activeIndex;
+    },
+  },
+  setup(props) {
     const progressCircle = ref(null);
     const onAutoplayTimeLeft = (s, time, progress) => {
       progressCircle.value.style.setProperty('--progress', 1 - progress);
@@ -52,8 +67,8 @@ export default {
     return {
       onAutoplayTimeLeft,
       progressCircle,
-      modules: [Autoplay, Navigation],
     };
   },
+
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
-    <Slider />
+    <Slider :datas=datas />
   </div>
 </template>
 <style>
-  h1{
-    text-align: center;
-  }
+h1 {
+  text-align: center;
+}
 </style>
+<script>
+export default {
+  data() {
+    return {
+      datas: [{
+        img: 'image 1',
+        title: 'Unparalleled Quality',
+        description: 'Achieveing highly accurate transcription is our number one priority - without compromising speed (1h audio < 60s) while remaining one of the most affordable API providers on the market, add-ons included.'
+      }, {
+        img: 'image 2',
+        title:'100% Security',
+        description: `We take our client’s privacy very seriously. Our data hosting is fully compliant with all EU and US regulations, and our portfolio is constantly updated with the latest security add-ons like PII redaction to guarantee full legal compliance.`
+      }, {
+        img: 'image 3',
+        title:'Multilingual Support',
+        description: 'Unlike any other provider on the market, our API supports 99 languages for transcription, translation, as well as all of the current and upcoming audio intelligence features.'
+      }, {
+        img: 'image 4',
+        title:'Easy Integration',
+        description: 'Our plug-and-play API is compatible with all existing tech stacks and does not require any AI expertise to integrate. Optimized for scalability, our enterprise-grade API is complemented by an intuitive in-app toggle system for the add-ons.'
+      }, {
+        img: 'image 5',
+        title:'Easy Integration',
+        description: 'Our plug-and-play API is compatible with all existing tech stacks and does not require any AI expertise to integrate. Optimized for scalability, our enterprise-grade API is complemented by an intuitive in-app toggle system for the add-ons.'
+      }],
+    }
+  }
+}
+</script>
